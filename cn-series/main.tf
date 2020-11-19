d7eda5bf9a1e187430d69acce0a389a1244da298
ssumner32 <30295405+ssumner32@users.noreply.github.com>
2020-11-19 15:19:17 -0800

    Update main.tf

--- cn-series/main.tf
+++ cn-series/main.tf
@@ -22,7 +22,7 @@ provider helm {}
 
 resource "helm_release" "cn-series" {
   name       = "cn-series-deploy"
-  repository = "https://paloaltonetworks.github.io/cn-series-hackathon-helm/"
+  repository = "https://paloaltonetworks.github.io/cn-series-helm/"
   chart      = "cn-series"
   version    = "0.1.5"
   timeout    = 600
